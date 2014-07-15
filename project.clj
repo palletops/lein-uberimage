dfda21e1fac5f0e99e83aaba0e72ebaa55556a4e
Hugo Duncan <hugo@palletops.com>
2014-07-15 16:39:56 +0000

    Updated version for next release cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.palletops/uberimage "0.1.1"
+(defproject com.palletops/uberimage "0.1.2-SNAPSHOT"
   :description "Leiningen plugin to create a docker image for a project uberjar"
   :url "http://github.com/palletops/lein-uberimage"
   :license {:name "Eclipse Public License"
